bcac948c85027e8e9c3cc19b47b84270c3b8a12e
kitaku <ill.white00xx@gmail.com>
2019-06-12 14:00:02 +0900

    more modefied

--- js/common.js
+++ js/common.js
@@ -24,18 +24,43 @@ $(function(){
     $('body,html').animate({scrollTop:position}, speed, 'swing');
     return false;
   });
-});
 
-$(function(){
   $('#ob-og').click(function(){
     $('.ob-og').modal('show');
   });
-})
 
-// $(function(){
-//   $('.tab_btn').on('click',function(){
-//     $('.tab_item').removeClass("is_active_item");
-//     // $($(this).attr("href")).addClass("is_active_item");
-//     $($(this).attr("href")).addClass("is_active_item";
-//   })
-// })
+  $('.tab-btn').on('click',function(){
+    $('.tab_btn').removeClass("active");
+    $('.tab-item').removeClass("active");
+
+    $($(this).attr("href")).addClass("active");
+    $(this).addClass("active");
+
+  });
+
+  var winwidth =
+
+  $('.ui.labeled.icon.sidebar').sidebar('toggle');
+
+  // var tag = document.createElement('script');
+  // tag.src = "http://www.youtube.com/iframe_api";
+  // var firstScriptTag = document.getElementByTagName('script')[0];
+  // firstScriptTag.parentNode.inserBefore(tag, firstScriptTag);
+  // $('.slides').slick({
+  //   accessibility: true,
+  //   speed: 400,
+  //   cssEase: 'ease',
+  //   dots: true,
+  //   draggable: true,
+  //   arrows: true,
+  //   infinite: true,
+  //   swipe: true,
+  // });
+  // $('.slides').on('afterChange', function(event, slick, currentSlide){
+  //   Object.keys(ytPlayers).forEach(function(key) {
+  //     var ytPlayer = this[key];
+  //     console.log(key, ytPlayer);
+  //     ytPlayer.pauseVideo();
+  //   }, ytPlayers);
+  // });
+});
